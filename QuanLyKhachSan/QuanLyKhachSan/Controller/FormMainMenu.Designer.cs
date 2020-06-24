077a4caaf7578b49f4c15a4e8b3c310d692e916a
D.Quân <manhquan0105@gmail.com>
2020-06-24 23:24:09 +0700

    sua form Main

--- QuanLyKhachSan/QuanLyKhachSan/Controller/FormMainMenu.Designer.cs
+++ QuanLyKhachSan/QuanLyKhachSan/Controller/FormMainMenu.Designer.cs
@@ -33,7 +33,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.quanLyVatDungToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quanLyPhongToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.quanLyKhachThueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.quanLyDichVuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.thanhToanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -45,9 +45,7 @@
             this.buttonThuePhong = new System.Windows.Forms.Button();
             this.buttonDichVu = new System.Windows.Forms.Button();
             this.buttonKhach = new System.Windows.Forms.Button();
-            this.buttonVatDung = new System.Windows.Forms.Button();
             this.buttonPhong = new System.Windows.Forms.Button();
-            this.quanLyPhongToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panel1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.panel6.SuspendLayout();
@@ -94,7 +92,6 @@
             this.menuToolStripMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.quanLyPhongToolStripMenuItem,
-            this.quanLyVatDungToolStripMenuItem,
             this.quanLyKhachThueToolStripMenuItem,
             this.quanLyDichVuToolStripMenuItem,
             this.thanhToanToolStripMenuItem,
@@ -104,38 +101,38 @@
             this.menuToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
             this.menuToolStripMenuItem.Text = "Menu";
             // 
-            // quanLyVatDungToolStripMenuItem
+            // quanLyPhongToolStripMenuItem
             // 
-            this.quanLyVatDungToolStripMenuItem.Name = "quanLyVatDungToolStripMenuItem";
-            this.quanLyVatDungToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
-            this.quanLyVatDungToolStripMenuItem.Text = "Quan ly vat dung ";
-            this.quanLyVatDungToolStripMenuItem.Click += new System.EventHandler(this.quanLyVatDungToolStripMenuItem_Click);
+            this.quanLyPhongToolStripMenuItem.Name = "quanLyPhongToolStripMenuItem";
+            this.quanLyPhongToolStripMenuItem.Size = new System.Drawing.Size(222, 26);
+            this.quanLyPhongToolStripMenuItem.Text = "Quan ly phong";
+            this.quanLyPhongToolStripMenuItem.Click += new System.EventHandler(this.quanLyPhongToolStripMenuItem_Click);
             // 
             // quanLyKhachThueToolStripMenuItem
             // 
             this.quanLyKhachThueToolStripMenuItem.Name = "quanLyKhachThueToolStripMenuItem";
-            this.quanLyKhachThueToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.quanLyKhachThueToolStripMenuItem.Size = new System.Drawing.Size(222, 26);
             this.quanLyKhachThueToolStripMenuItem.Text = "Quan ly khach thue";
             this.quanLyKhachThueToolStripMenuItem.Click += new System.EventHandler(this.quanLyKhachThueToolStripMenuItem_Click);
             // 
             // quanLyDichVuToolStripMenuItem
             // 
             this.quanLyDichVuToolStripMenuItem.Name = "quanLyDichVuToolStripMenuItem";
-            this.quanLyDichVuToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.quanLyDichVuToolStripMenuItem.Size = new System.Drawing.Size(222, 26);
             this.quanLyDichVuToolStripMenuItem.Text = "Quan ly dich vu";
             this.quanLyDichVuToolStripMenuItem.Click += new System.EventHandler(this.quanLyDichVuToolStripMenuItem_Click);
             // 
             // thanhToanToolStripMenuItem
             // 
             this.thanhToanToolStripMenuItem.Name = "thanhToanToolStripMenuItem";
-            this.thanhToanToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.thanhToanToolStripMenuItem.Size = new System.Drawing.Size(222, 26);
             this.thanhToanToolStripMenuItem.Text = "Hóa đơn";
             this.thanhToanToolStripMenuItem.Click += new System.EventHandler(this.thanhToanToolStripMenuItem_Click);
             // 
             // huongDanSuDungToolStripMenuItem
             // 
             this.huongDanSuDungToolStripMenuItem.Name = "huongDanSuDungToolStripMenuItem";
-            this.huongDanSuDungToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.huongDanSuDungToolStripMenuItem.Size = new System.Drawing.Size(222, 26);
             this.huongDanSuDungToolStripMenuItem.Text = "Huong dan su dung";
             this.huongDanSuDungToolStripMenuItem.Click += new System.EventHandler(this.huongDanSuDungToolStripMenuItem_Click);
             // 
@@ -164,7 +161,6 @@
             this.panel6.Controls.Add(this.buttonThuePhong);
             this.panel6.Controls.Add(this.buttonDichVu);
             this.panel6.Controls.Add(this.buttonKhach);
-            this.panel6.Controls.Add(this.buttonVatDung);
             this.panel6.Controls.Add(this.buttonPhong);
             this.panel6.Location = new System.Drawing.Point(3, 107);
             this.panel6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
@@ -178,7 +174,7 @@
             this.buttonThanhToan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonThanhToan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonThanhToan.ForeColor = System.Drawing.Color.White;
-            this.buttonThanhToan.Location = new System.Drawing.Point(53, 366);
+            this.buttonThanhToan.Location = new System.Drawing.Point(53, 326);
             this.buttonThanhToan.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonThanhToan.Name = "buttonThanhToan";
             this.buttonThanhToan.Size = new System.Drawing.Size(163, 39);
@@ -193,7 +189,7 @@
             this.buttonThuePhong.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonThuePhong.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonThuePhong.ForeColor = System.Drawing.Color.White;
-            this.buttonThuePhong.Location = new System.Drawing.Point(53, 234);
+            this.buttonThuePhong.Location = new System.Drawing.Point(53, 194);
             this.buttonThuePhong.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonThuePhong.Name = "buttonThuePhong";
             this.buttonThuePhong.Size = new System.Drawing.Size(163, 39);
@@ -208,7 +204,7 @@
             this.buttonDichVu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonDichVu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonDichVu.ForeColor = System.Drawing.Color.White;
-            this.buttonDichVu.Location = new System.Drawing.Point(53, 299);
+            this.buttonDichVu.Location = new System.Drawing.Point(53, 259);
             this.buttonDichVu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonDichVu.Name = "buttonDichVu";
             this.buttonDichVu.Size = new System.Drawing.Size(163, 39);
@@ -223,7 +219,7 @@
             this.buttonKhach.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonKhach.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonKhach.ForeColor = System.Drawing.Color.White;
-            this.buttonKhach.Location = new System.Drawing.Point(53, 167);
+            this.buttonKhach.Location = new System.Drawing.Point(53, 127);
             this.buttonKhach.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonKhach.Name = "buttonKhach";
             this.buttonKhach.Size = new System.Drawing.Size(163, 39);
@@ -232,28 +228,13 @@
             this.buttonKhach.UseVisualStyleBackColor = false;
             this.buttonKhach.Click += new System.EventHandler(this.buttonKhach_Click);
             // 
-            // buttonVatDung
-            // 
-            this.buttonVatDung.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.buttonVatDung.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonVatDung.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonVatDung.ForeColor = System.Drawing.Color.White;
-            this.buttonVatDung.Location = new System.Drawing.Point(53, 101);
-            this.buttonVatDung.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.buttonVatDung.Name = "buttonVatDung";
-            this.buttonVatDung.Size = new System.Drawing.Size(163, 39);
-            this.buttonVatDung.TabIndex = 1;
-            this.buttonVatDung.Text = "Vat Dung";
-            this.buttonVatDung.UseVisualStyleBackColor = false;
-            this.buttonVatDung.Click += new System.EventHandler(this.buttonVatDung_Click);
-            // 
             // buttonPhong
             // 
             this.buttonPhong.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.buttonPhong.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonPhong.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonPhong.ForeColor = System.Drawing.Color.White;
-            this.buttonPhong.Location = new System.Drawing.Point(53, 36);
+            this.buttonPhong.Location = new System.Drawing.Point(53, 61);
             this.buttonPhong.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonPhong.Name = "buttonPhong";
             this.buttonPhong.Size = new System.Drawing.Size(163, 39);
@@ -261,13 +242,6 @@
             this.buttonPhong.Text = "Phong";
             this.buttonPhong.UseVisualStyleBackColor = false;
             this.buttonPhong.Click += new System.EventHandler(this.buttonPhong_Click);
-            // 
-            // quanLyPhongToolStripMenuItem
-            // 
-            this.quanLyPhongToolStripMenuItem.Name = "quanLyPhongToolStripMenuItem";
-            this.quanLyPhongToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
-            this.quanLyPhongToolStripMenuItem.Text = "Quan ly phong";
-            this.quanLyPhongToolStripMenuItem.Click += new System.EventHandler(this.quanLyPhongToolStripMenuItem_Click);
             // 
             // FormMainMenu
             // 
@@ -301,12 +275,10 @@
         private System.Windows.Forms.Button buttonThuePhong;
         private System.Windows.Forms.Button buttonDichVu;
         private System.Windows.Forms.Button buttonKhach;
-        private System.Windows.Forms.Button buttonVatDung;
         private System.Windows.Forms.Button buttonPhong;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem thoatToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem quanLyVatDungToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem quanLyKhachThueToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem quanLyDichVuToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem thanhToanToolStripMenuItem;
